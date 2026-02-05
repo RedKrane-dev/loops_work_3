6deb269ae9d1e5d8ab3ab35b7866b536f914ff98
RedKrane86 <redkrane86@gmail.com>
2026-02-05 19:13:25 +0500

    fixes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import string
 
 def main():
-    text = input("Введите текст: ")
+    text = input("Введите текст:\n")
     text_low = text.lower()
 
     vowels = "аеёиоуыэюя"
@@ -38,12 +38,15 @@ def main():
         else:
             words_count_dict[word] = 1
 
-    print("Количество слов в тексте:", word_count)
-    print("Самое длинное слово:", longest_word if longest_word else "(нет слов)")
-    print("Количество гласных букв в тексте:", vowel_count)
-    print("Частота слов в тексте:")
+    print(
+        f'Количество слов в тексте: {word_count}',
+        f'Самое длинное слово: {longest_word if longest_word else "(нет слов)"}',
+        f'Количество гласных букв в тексте: {vowel_count}',
+        f'Частота слов в тексте:',
+        sep='\n'
+    )
     for word, count in words_count_dict.items():
-        print(f"{word}: {count}")
+        print(f"\t{word}: {count}")
 
 if __name__ == "__main__":
     main()
