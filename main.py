16ad43afd6277aad9975a063ee40b01131539b3e
RedKrane86 <redkrane86@gmail.com>
2026-02-05 19:14:28 +0500

    refactor

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
 import string
 
 def main():
-    text = input("Введите текст:\n")
+    text = input('Введите текст:\n')
     text_low = text.lower()
 
-    vowels = "аеёиоуыэюя"
+    vowels = 'аеёиоуыэюя'
     simbols = string.punctuation
 
     cleaned_chars = []
     for char in text_low:
         if char in simbols:
-            cleaned_chars.append(" ")
+            cleaned_chars.append(' ')
         else:
             cleaned_chars.append(char)
-    cleaned_text = "".join(cleaned_chars)
+    cleaned_text = ''.join(cleaned_chars)
     # 1
     words = cleaned_text.split()
     word_count = len(words)
 
     # 2
-    longest_word = ""
+    longest_word = ''
     for word in words:
         if len(word) > len(longest_word):
             longest_word = word
@@ -40,13 +40,13 @@ def main():
 
     print(
         f'Количество слов в тексте: {word_count}',
-        f'Самое длинное слово: {longest_word if longest_word else "(нет слов)"}',
+        f'Самое длинное слово: {longest_word if longest_word else '(нет слов)'}',
         f'Количество гласных букв в тексте: {vowel_count}',
         f'Частота слов в тексте:',
         sep='\n'
     )
     for word, count in words_count_dict.items():
-        print(f"\t{word}: {count}")
+        print(f'\t{word}: {count}')
 
 if __name__ == "__main__":
     main()
